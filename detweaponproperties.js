7f97dd7be6aea69ba5096511ea226acb47f4e471
Detraya <pigottsm@gmail.com>
2022-01-17 20:12:49 -0600

    first commit

---
+++ detweaponproperties.js
@@ -0,0 +1,5 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['longloada'] = 'Longload (1 A or BA)';
+	CONFIG.DND5E.weaponProperties['longload1min'] = 'Longload (1 Minute)';
+	CONFIG.DND5E.weaponProperties['dry'] = 'Dry';
+});
