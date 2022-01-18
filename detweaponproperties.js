327b87e967d54f0a948dbcfc24dcaeca5ebf905b
Detraya <pigottsm@gmail.com>
2022-01-17 20:41:27 -0600

    "small bugfix"

--- detweaponproperties.js
+++ detweaponproperties.js
@@ -1,6 +1,6 @@
 Hooks.on('ready', () => {
 	CONFIG.DND5E.weaponProperties['longloada'] = 'Longload A';
-	CONFIG.DND5e.weaponProperties['longloadBA'] = 'Longload BA';
+	CONFIG.DND5e.weaponProperties['longloadba'] = 'Longload BA';
 	CONFIG.DND5E.weaponProperties['longload1min'] = 'Longload 1Min';
 	CONFIG.DND5E.weaponProperties['dry'] = 'Dry';
 });
