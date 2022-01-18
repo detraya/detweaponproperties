940e34891c6389e3f044ca5860d2411768600e3c
Detraya <pigottsm@gmail.com>
2022-01-17 20:57:35 -0600

    "oh god please"

--- detweaponproperties.js
+++ detweaponproperties.js
@@ -1,6 +1,6 @@
 Hooks.on('ready', () => {
 	CONFIG.DND5E.weaponProperties['longloada'] = 'Longload A';
-	CONFIG.DND5e.weaponProperties['longloadba'] = 'Longload BA';
+	CONFIG.DND5E.weaponProperties['longloadba'] = 'Longload BA';
 	CONFIG.DND5E.weaponProperties['longload1min'] = 'Longload 1Min';
 	CONFIG.DND5E.weaponProperties['dry'] = 'Dry';
 });
